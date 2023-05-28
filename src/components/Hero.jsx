bcc4926b1a062ce1f085a7454b9edbd6d8202928
SYEDUMAIR007 <92495743+SYEDUMAIR007@users.noreply.github.com>
2023-05-28 22:54:55 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { logo } from '../assets'
+
+const Hero = () => {
+  return (
+    <header className='w-full flex flex-col justify-center items-center '>
+      <nav className='w-full flex justify-between items-center  mb-10 pt-3'>
+        <img src={logo} alt="sumzlogo" className='w-28 object-contain' />
+        <button onClick={()=>window.open("https://github.com/SYEDUMAIR007")} className='black_btn'>GitHub
+        </button>
+      </nav>
+      <h1 className='head_text'>Summarize Articles with <br />
+      <span className='orange_gradient'>OpenAI GPT-4</span>
+      </h1>
+      <h2 className='desc'>Simplify your reading with Summize, an 
+      open-source article summarizer that transforms lengthy articles 
+      into clear and concise summaries
+      </h2>
+    </header>
+  )
+}
+
+export default Hero
